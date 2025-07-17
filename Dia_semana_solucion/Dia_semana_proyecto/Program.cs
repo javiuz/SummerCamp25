73a59386f3d2b45a9c64174ede7e9e4a22364667
Javier Zaratiegui <javizaraurda@outlook.com>
2025-07-17 09:59:38 +0200

    Programa para escribir el día de la semana

---
+++ Dia_semana_solucion/Dia_semana_proyecto/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Dia semanal");
+
+var nombredia = "";
+while(true) { 
+    Console.WriteLine("Introduce un número del 1 al 7 (o 0 para salir):"); 
+    var dia = Console.ReadLine();
+    // Si el nº es 0 salimos
+    if(dia == "0") { 
+        Console.WriteLine("Hasta Luego!");
+        break;
+    }
+    // Si el nº está entre 1 y 7 escribimos el día correspondiente de la semana
+    switch (dia) { 
+        case "1":
+            nombredia = "Lunes";
+            break;
+        case "2":
+            nombredia = "Martes";
+            break;
+        case "3":
+            nombredia = "Miércoles";
+            break;
+        case "4":
+            nombredia = "Jueves";
+            break;
+        case "5":
+            nombredia = "Viernes";
+            break;
+        case "6":
+            nombredia = "Sábado";
+            break;
+        case "7":
+            nombredia = "Domingo";
+            break;
+        default:
+            nombredia = "Día no válido";
+            break;
+    }
+    Console.WriteLine(nombredia); // Salida del nombre del día
+}
+
+
